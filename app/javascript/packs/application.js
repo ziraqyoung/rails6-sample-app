42f51fb3d46647228ff656d9337cd47f4689693f
ziraqyoung <ziraqyoung@outlook.com>
2020-05-02 17:10:00 +0300

    - Add stimulusjs - Create a profile dropdown in _header partial - Remove global underlined style for links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import '../stylesheets/application.scss'
+import '../stylesheets/application.scss';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +16,5 @@ import '../stylesheets/application.scss'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
