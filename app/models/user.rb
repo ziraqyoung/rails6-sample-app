9335226e852cd24efdde7fab87b3dbe2b089a653
ziraqyoung <ziraqyoung@outlook.com>
2020-05-19 09:17:28 +0300

    Micropost model - Generate model - Add associations and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   # or self.email = email.downcase (required during assignment)
   before_save { email.downcase! }
