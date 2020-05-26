913db03074c2a22ba20f274003d450f1b2d3a7c7
ziraqyoung <ziraqyoung@outlook.com>
2020-05-26 18:13:37 +0300

    Add Relationship model - user followers and following - Add test for user follow anf unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                           foreign_key: "follower_id",
+                           dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                           foreign_key: "followed_id",
+                           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   # or self.email = email.downcase (required during assignment)
   before_save { email.downcase! }
@@ -9,8 +17,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
@@ -71,9 +79,25 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # follows a user
+  def follow(other_user)
+    following << other_user
+  end
+
+  # unfollow a given user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if a user if following another user
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
